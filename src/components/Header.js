1a3f8adae8e9a180bd5a5b9289588e06dba26aec
ingridwy <bluebear589@gmail.com>
2020-07-23 17:24:18 +0100

    header stlying

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,30 @@
 import React from "react"
 import {Link} from "react-router-dom"
+import 'bootstrap/dist/css/bootstrap.css';
 
  class Header extends React.Component {
    render(){
     return (
-      <div class="header">
-        <Link to="/home">Home</Link>
-        <Link to="/about">About</Link>
-        <Link to="/projects">Projects</Link>
-      </div>
-    )
+    <div className="header">
+      <nav class="navbar navbar-expand-lg navbar-light bg-light" >
+        <h3>Ingrid Cheng</h3>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <a class="nav-link flex-fill" >
+            <Link to="/home">Home</Link>
+            <span class="sr-only">(current)</span></a>
+            <a class="nav-link flex-fill" >
+              <Link to="/about">About</Link>
+            </a>
+            <a class="nav-link flex-fill">
+              <Link to="/projects">Projects</Link>
+            </a>  
+        </div>
+      </nav>   
+    </div>
+        )
    }
   
 }
