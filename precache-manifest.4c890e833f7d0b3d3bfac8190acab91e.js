14d36047eb4345ac5ad55cc6d69d355a5cf911a2
ingridwy <bluebear589@gmail.com>
2020-07-27 02:18:26 +0100

    Updates

---
+++ precache-manifest.4c890e833f7d0b3d3bfac8190acab91e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d16b411f8c815655c45e697a55918f3c",
+    "url": "/ingridwy/ingrid-portfolio/index.html"
+  },
+  {
+    "revision": "0108a286131487f44c71",
+    "url": "/ingridwy/ingrid-portfolio/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "3eaa527be6c3a714e956",
+    "url": "/ingridwy/ingrid-portfolio/static/css/main.2a0fdf62.chunk.css"
+  },
+  {
+    "revision": "0108a286131487f44c71",
+    "url": "/ingridwy/ingrid-portfolio/static/js/2.68f8ec39.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ingridwy/ingrid-portfolio/static/js/2.68f8ec39.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3eaa527be6c3a714e956",
+    "url": "/ingridwy/ingrid-portfolio/static/js/main.2be82dbe.chunk.js"
+  },
+  {
+    "revision": "707eecba4d18b977e58c",
+    "url": "/ingridwy/ingrid-portfolio/static/js/runtime-main.5dc64974.js"
+  }
+]);
