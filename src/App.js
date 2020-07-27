bd4a4a8fff21829073f8a647e4505e7a36609000
ingridwy <bluebear589@gmail.com>
2020-07-27 02:33:28 +0100

    typo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './App.css';
-import { Home }from './components/Home'
 import { About } from './components/About'
 import { Projects } from './components/Projects'
 
