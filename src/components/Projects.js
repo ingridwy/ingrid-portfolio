989a83acf8ac3c5f8c3fb6e51fd417fd35a5157c
ingridwy <bluebear589@gmail.com>
2020-07-27 02:11:36 +0100

    2ndand third project content added

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -13,36 +13,71 @@ class Projects extends React.Component {
   }
   render() {
     const projectInfo = this.state.projects.map(project => project={project})
-    var eachProject = projectInfo[0].project
-    
+
+    var eachProject = projectInfo[this.state.index].project
+    const secondProject = projectInfo[1].project
+    const thirdProject = projectInfo[2].project
     return (
       <div>
         <Header />
-        <div className="projects-container">
-        <h4>{`${eachProject.name}`}</h4>
-        <p>Website: 
-          <a href={`${eachProject.url}`} target="_blank" rel="noopener noreferrer" className="project-link">
-            {` ${eachProject.url}`}
-          </a>
-          </p>
-        <p>Github: 
-          <a href={`${eachProject.github}`} target="_blank" rel="noopener noreferrer" className="project-link">{` ${eachProject.github}`}</a>
-          </p>
-        <p>Tech: {`${eachProject.skills.join(" ○ ")}`}</p>
-        <div className="image-container">
-        {eachProject.img.map((img, idx) => (
-          
-            <img src={img} alt="" className="project-image" key={idx} />
-    
-          ))}
-        </div>
-        </div>
         
+        <div className="projects-container">
+          <h4>{`${eachProject.name}`}</h4>
+          <p>Website: 
+            <a href={`${eachProject.url}`} target="_blank" rel="noopener noreferrer" className="project-link">
+              {` ${eachProject.url}`}
+            </a>
+          </p>
+          <p>Github: 
+            <a href={`${eachProject.github}`} target="_blank" rel="noopener noreferrer" className="project-link">{` ${eachProject.github}`}</a>
+          </p>
+          <p>Tech: {`${eachProject.skills.join(" ○ ")}`}</p>
+          <p>{`${eachProject.description}`}</p>
+          <div className="image-container">
+          {eachProject.img.map((img, idx) => (
+              <img src={img} alt="" className="project-image" key={idx} />
+            ))}
+          </div>
+        </div>
 
+        <div className="projects-container">
+          <h4>{`${secondProject.name}`}</h4>
+          <p>Website: 
+            <a href={`${secondProject.url}`} target="_blank" rel="noopener noreferrer" className="project-link">
+              {` ${secondProject.url}`}
+            </a>
+          </p>
+          <p>Github: 
+            <a href={`${secondProject.github}`} target="_blank" rel="noopener noreferrer" className="project-link">{` ${eachProject.github}`}</a>
+          </p>
+          <p>Tech: {`${secondProject.skills.join(" ○ ")}`}</p>
+          <p>{`${secondProject.description}`}</p>
+          <div className="image-container">
+          {secondProject.img.map((img, idx) => (
+              <img src={img} alt="" className="project-image" key={idx} />
+            ))}
+          </div>
+        </div> 
 
-          
-         
-       
+          <div className="projects-container">
+          <h4>{`${thirdProject.name}`}</h4>
+          <p>Website: 
+            <a href={`${thirdProject.url}`} target="_blank" rel="noopener noreferrer" className="project-link">
+              {` ${thirdProject.url}`}
+            </a>
+          </p>
+          <p>Github: 
+            <a href={`${thirdProject.github}`} target="_blank" rel="noopener noreferrer" className="project-link">{` ${eachProject.github}`}</a>
+          </p>
+          <p>Tech: {`${thirdProject.skills.join(" ○ ")}`}</p>
+          <p>{`${thirdProject.description}`}</p>
+          <div className="image-container">
+          {thirdProject.img.map((img, idx) => (
+              <img src={img} alt="" className="project-image" key={idx} />
+            ))}
+          </div>
+        </div>
+     
         <Footer />
       </div>
 
--- src/components/data.js
+++ src/components/data.js
@@ -1,7 +1,6 @@
 export const projectsData = [
     {
-      id:1,
-      img: ["/image/dateindex.png","/image/dateplan.png"],
+      img: ["/image/datehomepage.png","/image/dateindex.png","/image/dateshow.png","/image/dateplan.png"],
       name: "Dating Guru",
       url: "http://www.dating-guru.site",
       github: "https://github.com/ingridwy/dating_guru",
@@ -12,11 +11,12 @@ export const projectsData = [
         "Ruby on Rails",
         "Bootstrap",
         "PostgreSQL"
-      ]
+      ],
+      description:"Enable couples to plan a date by browsing and selecting a restaurant plus an activity of their choice. A plan with details of venues and the route for commuting will be generated. The app holds records of previous date plans using PostgreSQL. Users can leave comments with their username and avatar shown."
     },
+    
     {
-      id:2,
-        img: ["/image/renthomepng", "/image/rentindex.png","/image/rentindex2.png"],
+        img: ["/image/renthome.png", "/image/rentindex.png","/image/rentindex2.png"],
         name: "Rent Dress",
         url: "https://rentdress.herokuapp.com/",
         github: "https://github.com/AkaneF/rent_dress",
@@ -27,7 +27,22 @@ export const projectsData = [
           "Ruby on Rails",
           "Bootstrap",
           "PostgreSQL"
-        ]
+        ],
+        description: "RentDress is a B2B lending platform for users to rent dress from others and review experience. A map is provided to show location of the lender. Users can click on photo to see enlarge image or suggested items within same category."
+      },
+      {
+        img: ["/image/quiz.png"],
+        name: "FunQuizz",
+        url: "https://funquizzz.herokuapp.com/",
+        github: "https://github.com/AkaneF/rent_dress",
+        skills: [
+          "HTML",
+          "CSS",
+          "Vue.js",
+          "JavaScript",
+          "Bootstrap"
+        ],
+        description: "FunQuizzz is a single page web application with 10 questions about film. Users can select answers, after submitting, the app will show the correct answers and count the score. New setof quizz will appear if the 'Next Question' button is pressed after the 10th question." 
       }
 ]
 export default projectsData;
